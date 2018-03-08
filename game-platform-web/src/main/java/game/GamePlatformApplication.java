105f5fd180f0cfad1fb604bcc02ae6f06adaf076
Amen <362961910@qq.com>
2018-03-08 12:15:43 +0800

    init

---
+++ game-platform-web/src/main/java/game/GamePlatformApplication.java
@@ -0,0 +1,13 @@
+package game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamePlatformApplication.class, args);
+	}
+
+}
